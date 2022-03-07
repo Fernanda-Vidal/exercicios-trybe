f684363e1d72f07b8cae8003d6cbfecfade9ff22
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-07 20:00:19 -0300

    Exercício 3.

--- fundamentos/bloco-04/dia-4.1/script.js
+++ fundamentos/bloco-04/dia-4.1/script.js
@@ -7,12 +7,26 @@
 // console.log(a / b);
 // console.log(a % b);
 
+// const num1 = 20;
+// const num2 = 30;
+
+// if (num1 > num2) {
+//     console.log(num1);
+// }
+// else {
+//     console.log (num2);
+// }
+
 const num1 = 20;
 const num2 = 30;
+const num3 = 150;
 
-if (num1 > num2) {
+if (num1 > num2 && num1 > num3) {
     console.log(num1);
+} 
+else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
 }
 else {
-    console.log (num2);
+    console.log(num3)
 }
