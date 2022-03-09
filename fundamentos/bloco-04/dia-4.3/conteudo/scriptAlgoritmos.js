fed330eacb7c40285b82c1eb49a0af6ea3f4491a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-09 19:46:42 -0300

    Adiciona algoritmo do exercício 4.2.

---
+++ fundamentos/bloco-04/dia-4.3/conteudo/scriptAlgoritmos.js
@@ -0,0 +1,46 @@
+// Utilizando o array abaixo, percorra-o somando todos os valores.Caso o valor final seja maior 
+// que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
+
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+
+for (let i = 0; i < fruits.length; i += 1) {
+    soma += fruits[i];
+}
+
+    if (soma > 15) {
+        console.log(soma);
+    }
+    else {
+        console.log("Menor que 16");
+    }
+// Volte aos exercícios do dia anterior, 4.2, e crie um algoritmo para cada.
+
+// 1-Utilize o array numbers;
+// 2-Utilize o for para percorrer o array;
+// 3-Imprima os valores usando console.log;
+// 4-Some os valores contidos no array através do incremento;
+// 5-Imprima o resultado com console.log;
+// 6-Utiliza o .legth para verificar o tamanho do array;
+// 7-Utilize a variável da soma dos valores do array e divida pelo tamanho do array;
+// 8-Utiliza if para imprimir resultado baseado na média aritmética encontrada acima;
+// 9-Percorra o array number com o for;
+// 10-Usar o if com o conteúdo do índice 0 para comparar com os outros indíces e substituí-lo
+// toda vez que encontrar um número maior;
+// 11-imprimir o maior número do array com console.log;
+// 12-Percorrer o array com for;
+// 13-Aplicar o módulo;
+// 14-Salvar os impares em uma variável;
+// 15-Usar console.log para imprimir o resultado conforme solicitado;
+// 16-Percorra o array com o for;
+// 17-Utilize if para comparar o valor do índice 0 e descobrir se existem números menores que ele;
+// 18-Utilize console.log para imprimir o resultado solicitado;
+// 19-Crie um array vazio;
+// 20-Utilize o for para inserir valores no array vazio;
+// 21-Utiliza o .push;
+// 22-Utiliza console.log para imprimir o resultado;
+// 23-Percorra o array criado acima com o for;
+// 24-Utilize o operador aritmético de divisão para dividir o valor de cada índice por 2;
+// 25-Salve o resultado em uma variável;
+// 26-Imprima a variável.
