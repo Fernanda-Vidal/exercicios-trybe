72ece2f99884e6330bbe9a5aa520ec1d5fa67310
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-10 16:03:05 -0300

    Commit inicial de exercicio 1 e 2 do conteúdo 4.4.

---
+++ fundamentos/bloco-04/dia-4.4/conteudo/objetos.js
@@ -0,0 +1,16 @@
+// 1 - Crie um objeto player contendo as variáveis listadas abaixo.
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+}
+
+// 2 - Acesse as chaves name , lastName e age e concatene as suas informações para
+// imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem
+//  34 anos de idade".
+console.log('A jogadora '+ player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+// 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo
+//  as datas em que a jogadora Marta foi considerada a melhor do mundo.
