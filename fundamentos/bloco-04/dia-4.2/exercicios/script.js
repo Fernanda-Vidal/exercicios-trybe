176151aa4ff078e0f0cf1b399347eebe96b948df
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-09 21:49:48 -0300

    Adiciona exercício 3.

--- fundamentos/bloco-04/dia-4.2/exercicios/script.js
+++ fundamentos/bloco-04/dia-4.2/exercicios/script.js
@@ -13,13 +13,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // 2) Para o segundo exercício, some todos os valores contidos no array e imprima o resultado:
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let i = 0; i < numbers.length; i += 1){
     soma += numbers[i];
 }
 
 console.log(soma);
+
+// 3) Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let mediaAritmetica = soma / numbers.length;
+
+console.log(mediaAritmetica);
+
+
 
 
 
