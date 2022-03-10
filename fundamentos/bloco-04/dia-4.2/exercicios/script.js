e0e8be5bef742439e8c94b55ac37f78a681af72a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-09 21:36:36 -0300

    Adiciona exercício 2.

--- fundamentos/bloco-04/dia-4.2/exercicios/script.js
+++ fundamentos/bloco-04/dia-4.2/exercicios/script.js
@@ -2,14 +2,26 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(index = 0; index < numbers.length; index += 1){
-    console.log (numbers[index]);
-}
+// for(index = 0; index < numbers.length; index += 1){
+//     console.log (numbers[index]);
+// }
 
-for (let number of numbers){
-    console.log(number);
-}
+// // ou
+
+// for (let number of numbers){
+//     console.log(number);
+// }
 
 // 2) Para o segundo exercício, some todos os valores contidos no array e imprima o resultado:
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1){
+    soma += numbers[i];
+}
+
+console.log(soma);
+
+
+
 
 
