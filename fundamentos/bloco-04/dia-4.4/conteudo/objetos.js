e8294e20b79f821946bfddae88e358e13f32c0bf
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-10 18:06:52 -0300

    Adiciona exercício 3.

--- fundamentos/bloco-04/dia-4.4/conteudo/objetos.js
+++ fundamentos/bloco-04/dia-4.4/conteudo/objetos.js
@@ -14,3 +14,7 @@ console.log('A jogadora '+ player.name + ' ' + player.lastName + ' tem ' + playe
 
 // 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo
 //  as datas em que a jogadora Marta foi considerada a melhor do mundo.
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(player);
+
