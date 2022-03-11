eb96880a5175f2f411031f10b54963b03412864f
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-10 23:11:03 -0300

    Adiciona exercicio 2 de funçoes.

--- fundamentos/bloco-04/dia-4.4/conteudo/funcoes.js
+++ fundamentos/bloco-04/dia-4.4/conteudo/funcoes.js
@@ -1,39 +1,53 @@
 // 1 -Faça cinco programas, um para cada operação aritmética básica. Seu 
 // programa deve ter duas constantes, a e b , definidas no começo com 
 // os valores que serão operados. Faça programas para:
-function soma(par1, par2){
-    return(par1 + par2);
+function soma(par1, par2) {
+    return (par1 + par2);
 };
 
-console.log(soma(2,4));
+console.log(soma(2, 4));
 
 
 
-function subtracao(par1, par2){
-    return(par1 - par2);
+function subtracao(par1, par2) {
+    return (par1 - par2);
 };
 
-console.log(subtracao(10,5));
+console.log(subtracao(10, 5));
 
 
 
-function mutiplicacao(par1, par2){
-    return(par1 * par2);
+function mutiplicacao(par1, par2) {
+    return (par1 * par2);
 };
 
-console.log(mutiplicacao(3,9));
+console.log(mutiplicacao(3, 9));
 
 
 
-function divisao(par1, par2){
-    return(par1 / par2);
+function divisao(par1, par2) {
+    return (par1 / par2);
 };
 
-console.log(divisao(100,5));
+console.log(divisao(100, 5));
 
 
-function modulo(par1, par2){
-    return(par1 % par2);
+
+function modulo(par1, par2) {
+    return (par1 % par2);
 };
 
-console.log(modulo(354,2));
+console.log(modulo(354, 2));
+
+// 2-Faça um programa que retorne o maior de dois números. Defina no 
+// começo do programa duas constantes com os valores que serão 
+// comparados.
+function maiorNumero(num1, num2) {
+    if (num1 > num2) {
+        return(num1);
+    }
+    else {
+        return(num2);
+    };
+}
+console.log(maiorNumero(10, 20));
