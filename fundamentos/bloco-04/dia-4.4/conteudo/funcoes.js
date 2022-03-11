7820d4b2f4093819b47014276aa3de96f38a4090
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-10 23:00:57 -0300

    Adiciona exercício conteudo funcoes.

---
+++ fundamentos/bloco-04/dia-4.4/conteudo/funcoes.js
@@ -0,0 +1,39 @@
+// 1 -Faça cinco programas, um para cada operação aritmética básica. Seu 
+// programa deve ter duas constantes, a e b , definidas no começo com 
+// os valores que serão operados. Faça programas para:
+function soma(par1, par2){
+    return(par1 + par2);
+};
+
+console.log(soma(2,4));
+
+
+
+function subtracao(par1, par2){
+    return(par1 - par2);
+};
+
+console.log(subtracao(10,5));
+
+
+
+function mutiplicacao(par1, par2){
+    return(par1 * par2);
+};
+
+console.log(mutiplicacao(3,9));
+
+
+
+function divisao(par1, par2){
+    return(par1 / par2);
+};
+
+console.log(divisao(100,5));
+
+
+function modulo(par1, par2){
+    return(par1 % par2);
+};
+
+console.log(modulo(354,2));
