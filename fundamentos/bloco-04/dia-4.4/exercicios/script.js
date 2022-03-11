740cd0f0e363b955c10e35c859de162460575178
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-11 13:41:44 -0300

    Primeiro commit do exercício 4.4 .

---
+++ fundamentos/bloco-04/dia-4.4/exercicios/script.js
@@ -0,0 +1,15 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+//   1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, 
+// incluindo seu nome. Valor esperado no console:
+console.log('Bem-vinda, ' + info.personagem + '!');
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e 
+// o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+info['recorrente'] = 'Sim';
+
+console.log(info);
