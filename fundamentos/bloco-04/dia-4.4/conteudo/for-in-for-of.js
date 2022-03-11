368f928208e33ccea1c26a0348423dca02fdc30f
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-10 22:26:15 -0300

    Adiciona 'para fixar 2'.

--- fundamentos/bloco-04/dia-4.4/conteudo/for-in-for-of.js
+++ fundamentos/bloco-04/dia-4.4/conteudo/for-in-for-of.js
@@ -54,3 +54,15 @@ let names = {
   for(let name in names){
     console.log('Olá ' + names[name]);
   }
+
+//   2 - Usando o objeto abaixo, utilize for/in e imprima um 
+// console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  for(marca in car){
+      console.log(marca + ':', car[marca]);
+  };
