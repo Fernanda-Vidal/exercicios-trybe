2eda5b74a14e9021bee76dac6257674467b56268
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-14 12:18:16 -0300

    Adiciona conteudo parte III.

---
+++ fundamentos/bloco-05/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("h1")[0].style.color = "green";
+document.getElementsByClassName("emergency-tasks")[0].style.color = "orange";
+document.getElementById("footer-container").style.color = "blue";
+document.getElementsByTagName("h3")[3].style.color = "pink";
+document.querySelector("body").style.color = "yellow";
+// Porque o querySelector alterou a cor do meu segundo h3? Não deveria ser o primeiro elemento da tag informada?
