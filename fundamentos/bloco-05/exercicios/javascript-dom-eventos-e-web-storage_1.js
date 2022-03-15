4977122aa9c20bb449151919416ce3820f80a712
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-15 10:56:03 -0300

    Altera comandos para funções.

--- fundamentos/bloco-05/exercicios/javascript-dom-eventos-e-web-storage_1.js
+++ fundamentos/bloco-05/exercicios/javascript-dom-eventos-e-web-storage_1.js
@@ -1,15 +1,33 @@
-document.getElementsByTagName("p")[0].innerText = "Eu me vejo trabalhando com programação, na senioridade pleno ou quase lá, ganhando pelo menos 2x mais do que ganhava como técnica de seguro.".toUpperCase();
-document.getElementsByTagName("main")[0].style.backgroundColor = "rgb(76,164,109)";
-document.getElementsByTagName("section")[0].style.backgroundColor = "white";
-document.getElementsByClassName("title")[0].innerText = "Exercício 5.1 - JavaScript".toUpperCase();
-document.getElementsByTagName("p")[2].innerText = "Trybe".toUpperCase();
-
-function tagsP(){
-    let variav = document.getElementsByTagName("main");
-
-    for (let i = 0; i < variav.length; i += 1){
-        let nova = variav[i];
-    }
-    console.log([nova]);
+function mudaTexto(){
+    let paragrafo = document.getElementsByTagName("p");
+    paragrafo[0].innerText = 
+    "Eu me vejo trabalhando com programação, na senioridade pleno ou quase lá, ganhando pelo menos 2x mais do que ganhava como técnica de seguro.";
 }
-console.log(tagsP());
+mudaTexto();
+
+function quadradoAmarelo(cor){
+    let quadradoVerde = document.getElementsByTagName("main");
+    quadradoVerde[0].style.backgroundColor = cor;
+}
+quadradoAmarelo('rgb(76,164,109)');
+
+function quadradoVermelho (cor) {
+    let quadradoBranco = document.getElementsByClassName("center-content");
+    quadradoBranco[0].style.backgroundColor = cor;
+}
+quadradoVermelho('white');
+
+function corrigeTexto(elemento, texto){
+    let local = document.getElementsByClassName(elemento);
+    local[0].innerText = texto;
+}
+corrigeTexto("title", "Exercício 5.1 - JavaScript");
+
+function letrasMaiusculas (elemento){
+    let letras = document.getElementsByTagName(elemento);
+    for(let i = 0; i < letras.length; i += 1){
+        letras[i].innerText = letras[i].innerText.toUpperCase();
+    }
+}
+letrasMaiusculas('p');
+
