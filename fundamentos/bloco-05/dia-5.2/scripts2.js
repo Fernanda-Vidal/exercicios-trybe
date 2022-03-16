40bde02fb40be2234dcb8624e6f12b673297308c
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-16 09:11:19 -0300

    versionando

--- fundamentos/bloco-05/dia-5.2/scripts2.js
+++ fundamentos/bloco-05/dia-5.2/scripts2.js
@@ -1,11 +1,11 @@
 // 1)Crie um irmão para elementoOndeVoceEsta.
 function criaIrmao (elemento){
-    let enderecoIrmao = document.getElementById(elemento);
+    let enderecoIrmao = document.getElementById(elemento).parentElement;
     let irmao = document.createElement('id');
-    irmao.innerText = "teste";
+    irmao.innerText = "teste5555555555";
     enderecoIrmao.appendChild(irmao);
 }
-criaIrmao('pai');
+criaIrmao('elementoOndeVoceEsta');
 
 // 2)Crie um filho para elementoOndeVoceEsta.
 function criaFilho(){
@@ -19,11 +19,19 @@ criaFilho();
 // 3)Crie um filho para primeiroFilhoDoFilho.
 function neto(){
     let enderePai = document.getElementById('primeiroFilhoDoFilho');
-    let criaNeto = document.createElement('id');
+    let criaNeto = document.createElement('section');
     criaNeto.innerText = "Tudo caminhando muito bem!";
     enderePai.appendChild(criaNeto);
 }
 neto();
 
 // 4)A partir desse filho criado, acesse terceiroFilho.
-document.getElementsByTagName('id')
+document.getElementsByTagName('section')[4].parentElement.parentElement.nextElementSibling;
+
+
+// III - Removendo elementos
+
+// Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e 
+// primeiroFilhoDoFilho.
+
+
