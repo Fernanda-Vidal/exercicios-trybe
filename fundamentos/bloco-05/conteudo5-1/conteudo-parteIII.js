55aeaabb2733fa46ffb81ee519ba23cd6eeeecfa
Fernanda Vidal <99993299+Fernanda-Vidal@users.noreply.github.com>
2022-03-18 16:04:39 -0300

    Delete fundamentos/bloco-05/conteudo5-1 directory

--- fundamentos/bloco-05/conteudo5-1/conteudo-parteIII.js
+++
@@ -1,7 +0,0 @@
-const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
-document.querySelector("h2").innerText = "Curso Trybe";
-document.querySelectorAll("p")[1].innerText = "Aprendendo a manipular o Dom para realizar a interação do JavaScript com o HTML e o CSS e ficando com muitos cabelos brancos.";
-document.querySelector("h4").innerText = "Módulo 5.1 - parte3";
-document.querySelector("p").style.color = "orange";
-document.querySelector("h4").style.color = "pink";
