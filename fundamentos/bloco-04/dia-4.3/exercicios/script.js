3cd73a89a30955a7dea058d0ce1e5603f883e529
Fernanda Vidal <fevidal.dev@gmail.com>
2022-03-28 15:07:54 -0300

    .

--- fundamentos/bloco-04/dia-4.3/exercicios/script.js
+++ fundamentos/bloco-04/dia-4.3/exercicios/script.js
@@ -11,12 +11,13 @@ for (let i = fatorial; i > 0; i -= 1) {
 
 console.log(resultado);
 
+
 // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo,
 // a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como
 // exemplo, depois troque por outras para verificar se seu algoritmo está funcionando
 // corretamente.
 
-let word = 'fernanda';
+let word = 'tryber';
 let newWord = "";
 
 for (let i = word.length - 1; i >= 0; i -= 1) {
@@ -61,7 +62,7 @@ for(let numeroAtual = 4; numeroAtual <= n; numeroAtual += 1){
     }
 }
 
-console.log(numerosPrimos);
+// console.log(numerosPrimos);
 
 let maior = 2;
 for(let i = 0; i < numerosPrimos.length; i += 1){
