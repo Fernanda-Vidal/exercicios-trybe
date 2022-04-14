97dc2d191dd31768933b11a2d940b61dbe3236b0
Fernanda Vidal <fevidal.dev@gmail.com>
2022-04-13 22:36:06 -0300

    Adiciona exercicio 3.

--- fundamentos/bloco-08/dia-8.4/exercicios/exercise3.js
+++ fundamentos/bloco-08/dia-8.4/exercicios/exercise3.js
@@ -64,12 +64,29 @@ const books = [
   // Adicione o código do exercício aqui:
 //   3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
 
-function averageAge() {
-    let length = books.length;
-    let sum = books.reduce((acc, book) => (
-        acc + (book.releaseYear - book.author.birthYear)
-    ), 0);
-    return sum / length;
-  }
+// function averageAge() {
+//   const age = books.reduce((acc, book) => (acc + (book.releaseYear - book.author.birthYear)), 0) / books.length;
+//   return age;
+// }
 
-  console.log(averageAge())
+// // OU 
+
+// function averageAge() {
+//   const age = books.map((age) => age.releaseYear -  age.author.birthYear);
+//   const sum = age.reduce((acc, age) => (acc + age), 0);
+//   return sum / books.length;
+// }
+
+// OU 
+
+function averageAge() {
+  const average = books.reduce((acc, book) => {
+      const age = book.releaseYear - book.author.birthYear;
+      const sum = acc;
+      const medium = age / books.length;
+      return sum + medium;
+  }, 0)
+  return average;
+}
+
+console.log(averageAge());
