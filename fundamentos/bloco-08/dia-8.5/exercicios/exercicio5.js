c4fc980cec446f3d1864cd3ce7c03a1d86a8ffe6
Fernanda Vidal <fevidal.dev@gmail.com>
2022-04-16 11:09:40 -0300

    Adiciona exercicio 5 de 8.5.

---
+++ fundamentos/bloco-08/dia-8.5/exercicios/exercicio5.js
@@ -0,0 +1,7 @@
+// 5 - Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+
+const myList = [1, 2, 3];
+
+const swap = ([fst, snd, trd]) => [trd, snd, fst]
+
+console.log(swap(myList))
