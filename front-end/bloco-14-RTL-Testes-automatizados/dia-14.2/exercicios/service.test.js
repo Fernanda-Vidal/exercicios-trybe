110caccffa8a9db1356bec026634a89eeb4869bf
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-02 20:46:05 -0300

    Adiciona exercicio 2.

--- front-end/bloco-14-RTL-Testes-automatizados/dia-14.2/exercicios/service.test.js
+++ front-end/bloco-14-RTL-Testes-automatizados/dia-14.2/exercicios/service.test.js
@@ -1,9 +1,8 @@
 let { randomNumber } = require('./service');
 
 describe('Testa a função service', () => {
-    randomNumber = jest.fn().mockReturnValue(10);
-
-    it('Verifica se a função existe', () => {
+    it('Verifica se a função existe, foi chamada e quantas', () => {
+        randomNumber = jest.fn().mockReturnValue(10);
         expect(randomNumber()).toBe(10);
     })
 
@@ -17,3 +16,18 @@ describe('Testa a função service', () => {
         expect(randomNumber).toHaveBeenCalledTimes(3)
     })
 })
+
+describe('Verifica se a nova implementação funciona', () => {
+    it('Verifica se a divisão é aplicada, se a nova função é chamada e se ela é chamada somente uma vez', () => {
+        randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b)
+        
+        expect(randomNumber(10,5)).toBe(2)
+        expect(randomNumber).toHaveBeenCalled()
+        expect(randomNumber).toHaveBeenCalledTimes(1)
+    })
+
+    it('Verifica se a função é chamada com 10 e 5', () => {
+        expect(randomNumber).toHaveBeenCalledWith(10, 5)
+    })
+})
+
