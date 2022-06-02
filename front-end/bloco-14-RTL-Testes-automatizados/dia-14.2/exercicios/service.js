43db363c22f9a60a59ed9ee8ba25f8935d5d9403
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-02 19:18:44 -0300

    Adiciona exercicio 1.

---
+++ front-end/bloco-14-RTL-Testes-automatizados/dia-14.2/exercicios/service.js
@@ -0,0 +1,5 @@
+const randomNumber = () => Math.random() * 100 + 1;
+
+console.log(randomNumber())
+
+module.exports = { randomNumber };
---
+++ front-end/bloco-14-RTL-Testes-automatizados/dia-14.2/exercicios/service.test.js
@@ -0,0 +1,14 @@
+let { randomNumber } = require('./service');
+
+describe('Testa a função service', () => {
+    randomNumber = jest.fn().mockReturnValue(10);
+
+    it('Verifica se a função existe', () => {
+        expect(randomNumber()).toBe(10);
+    })
+
+    it('Verifica se a função foi chamada', () => {
+        randomNumber()
+        expect(randomNumber).toHaveBeenCalledTimes(2)
+    })
+})
