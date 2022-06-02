4809bf4635eb6ea3df922777a86a1aa072cf4158
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-02 20:49:37 -0300

    Adiciona exercicio 3.

--- front-end/bloco-14-RTL-Testes-automatizados/dia-14.2/exercicios/service.test.js
+++ front-end/bloco-14-RTL-Testes-automatizados/dia-14.2/exercicios/service.test.js
@@ -1,7 +1,7 @@
 let { randomNumber } = require('./service');
 
 describe('Testa a função service', () => {
-    it('Verifica se a função existe, foi chamada e quantas', () => {
+    it('Verifica se a função existe', () => {
         randomNumber = jest.fn().mockReturnValue(10);
         expect(randomNumber()).toBe(10);
     })
@@ -11,7 +11,7 @@ describe('Testa a função service', () => {
         expect(randomNumber).toHaveBeenCalled()
     })
 
-    it('Verifica se a função foi chamada', () => {
+    it('Verifica quantas vezes a função foi chamada', () => {
         randomNumber()
         expect(randomNumber).toHaveBeenCalledTimes(3)
     })
@@ -31,3 +31,25 @@ describe('Verifica se a nova implementação funciona', () => {
     })
 })
 
+describe('Verifica se a implementação com 3 parametros funciona', () => {
+    it('Verifica se a multiplicação dos parâmetros ocorre', () => {
+        randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c)
+
+        expect(randomNumber(2, 2, 2)).toBe(8)
+        expect(randomNumber).toHaveBeenCalled()
+        expect(randomNumber).toHaveBeenCalledTimes(1)
+        expect(randomNumber).toHaveBeenCalledWith(2, 2, 2)
+    })
+
+    it('Verifica se a função é resetada e a função que retorna o dobro é implementada', () => {
+        randomNumber.mockReset();
+        expect(randomNumber).toHaveBeenCalledTimes(0)
+
+        randomNumber = jest.fn().mockImplementation((a) => a * 2)
+        expect(randomNumber(2)).toBe(4)
+        expect(randomNumber).toHaveBeenCalledTimes(1)
+    })
+
+
+
+})
