9d791cd316695a6685966c172e7c5790bad5b3b0
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-04 14:12:52 -0300

    Adiciona exercício 2.

--- fundamentos/bloco-04/dia-4.4/exercicios/bonus.js
+++ fundamentos/bloco-04/dia-4.4/exercicios/bonus.js
@@ -5,37 +5,55 @@
 //  - se o maior estiver à direita, os dois números devem ser subtraídos;
 //  - se o maior estiver à esquerda, os dois números devem ser somados;
 
-const boardNumbers = {
-    I: 1, V: 5, X: 10, L: 50,
-    C: 100, D: 500, M: 1000,
-}
+// const boardNumbers = {
+//     I: 1, V: 5, X: 10, L: 50,
+//     C: 100, D: 500, M: 1000,
+// }
 
-const convertNumbers = (stringRomano) => {
-    stringRomano = stringRomano.toUpperCase();
+// const convertNumbers = (stringRomano) => {
+//     stringRomano = stringRomano.toUpperCase();
 
-    const array = stringRomano.split("")
-    let result = 0;
+//     const array = stringRomano.split("")
+//     let result = 0;
 
-    if (stringRomano.length === 1) {
-        return boardNumbers[stringRomano]
-    } else if (stringRomano.length === 2) {
-        for (let i = 0; i < array.length; i += 1) {
-            if (boardNumbers[array[i]] < boardNumbers[array[i + 1]]) {
-                result = boardNumbers[array[i + 1]] - boardNumbers[array[i]]
-                // return result;
-            } else {
-                result = boardNumbers[array[i]] + boardNumbers[array[i + 1]]
-                return result;
+//     if (stringRomano.length === 1) {
+//         return boardNumbers[stringRomano]
+//     } else if (stringRomano.length === 2) {
+//         for (let i = 0; i < array.length; i += 1) {
+//             if (boardNumbers[array[i]] < boardNumbers[array[i + 1]]) {
+//                 result = boardNumbers[array[i + 1]] - boardNumbers[array[i]]
+//                 // return result;
+//             } else {
+//                 result = boardNumbers[array[i]] + boardNumbers[array[i + 1]]
+//                 return result;
+//             }
+//         }
+//     } else {
+//         for (i in array) {
+//             result += boardNumbers[array[i]]
+//         }
+//         return result;
+//     }
+// }
+
+// console.log(convertNumbers('MMXVIII'));
+
+
+// EXERCÍCIO 2 -
+const list = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+const arrayOfNumbers = (vector) => {
+    let pares = [];
+    for (let i = 0; i < vector.length; i += 1) {
+        // console.log(vector[i])
+        for(let i2 = 0; i2 < vector[i].length; i2 += 1) {
+            if(vector[i][i2] % 2 === 0) {
+                pares.push(vector[i][i2])
             }
         }
-    } else {
-        for (i in array) {
-            result += boardNumbers[array[i]]
-        }
-        return result;
     }
+
+    return pares;
 }
 
-console.log(convertNumbers('MMXVIII'));
-
-
+console.log(arrayOfNumbers(list));
