a5c51f90ae54f6502f9838e9c084394690955a69
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-16 14:05:35 -0300

    Adiciona input email.

--- front-end/bloco-11-componentes-com-estado-eventos-forms/dia-11.2/exercicio/my-form-2.0/src/component/Forms.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-forms/dia-11.2/exercicio/my-form-2.0/src/component/Forms.jsx
@@ -3,20 +3,37 @@ import React from "react";
 class Forms extends React.Component {
     state = {
         inputValue: '',
+        inputEmail: '',
     }
-
-
-
+    
     handleChange = ({ target }) => {
-        this.setState({ inputValue: target.value.toUpperCase() })
+        const { name } = target;
+        if (name === 'inputValue') {
+            this.setState({ inputValue: target.value.toUpperCase() })
+        } else {           
+            const value = target.type === 'checkbox' ? target.checked : target.value;
+            
+            this.setState({
+                [name]: value,
+            })
+        }
     }
+
 
     render () {
-        const { inputValue } = this.state;
+        const { inputValue, inputEmail } = this.state;
         return(
             <form>
                 <fieldset>
+                    <label id="inputValue">
+                    Nome:
                     <input type="text" value={ inputValue } name="inputValue" maxLength="40" onChange={ this.handleChange } required/>
+                    </label>
+                    <br/>
+                    <label id="inputEmail">
+                    E-mail:
+                    <input type="email" value={ inputEmail } name="inputEmail" maxLength="50" onChange={ this.handleChange } required/>
+                    </label>
                 </fieldset>
             </form>
         )
