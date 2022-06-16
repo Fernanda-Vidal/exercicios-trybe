2f871e3f8e47841b8cb05461ef507d10466fb706
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-16 14:16:50 -0300

    Adiciona input CPF.

--- front-end/bloco-11-componentes-com-estado-eventos-forms/dia-11.2/exercicio/my-form-2.0/src/component/Forms.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-forms/dia-11.2/exercicio/my-form-2.0/src/component/Forms.jsx
@@ -4,6 +4,7 @@ class Forms extends React.Component {
     state = {
         inputValue: '',
         inputEmail: '',
+        inputCPF: 0,
     }
     
     handleChange = ({ target }) => {
@@ -21,19 +22,25 @@ class Forms extends React.Component {
 
 
     render () {
-        const { inputValue, inputEmail } = this.state;
+        const { inputValue, inputEmail, inputCPF } = this.state;
         return(
             <form>
                 <fieldset>
                     <label id="inputValue">
                     Nome:
-                    <input type="text" value={ inputValue } name="inputValue" maxLength="40" onChange={ this.handleChange } required/>
+                        <input type="text" value={ inputValue } name="inputValue" maxLength="40" onChange={ this.handleChange } required/>
                     </label>
                     <br/>
                     <label id="inputEmail">
                     E-mail:
-                    <input type="email" value={ inputEmail } name="inputEmail" maxLength="50" onChange={ this.handleChange } required/>
+                        <input type="email" value={ inputEmail } name="inputEmail" maxLength="50" onChange={ this.handleChange } required/>
                     </label>
+                    <br/>
+                    <label id="inputCPF">
+                    CPF:
+                        <input type="number" value={ inputCPF } name="inputCPF" maxLength="11" onChange={ this.handleChange } required />
+                    </label>
+
                 </fieldset>
             </form>
         )
