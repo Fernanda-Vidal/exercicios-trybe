264197cf6645de13c9572d175ba284ee99bb21fe
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-19 18:29:36 -0300

    Adiciona forms login.

--- front-end/bloco-15-Gerenciamento-estado-redux/dia-15.3/exercicio/my-app-redux/src/redux/page/Login.jsx
+++ front-end/bloco-15-Gerenciamento-estado-redux/dia-15.3/exercicio/my-app-redux/src/redux/page/Login.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
 class Login extends React.Component {
+    state = {
+        inputEmail: '',
+        inputPassword: '',
+    }
+
+    handleChange = ({ target }) => {
+        const { name } = target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+
+        this.setState({
+            [name]: value,
+        });
+    }
     render() {
+        const { inputEmail, inputPassword } = this.state;
         return (
-            <p>Tela de Login</p>
+            <div className='container-login'>
+                <form>
+                    <label>
+                    Email:
+                    <input type="email" value={ inputEmail } name="inputEmail" onChange={ this.handleChange } />
+                    </label>
+                    <br/>
+                    <label>
+                    Password:
+                    <input type="password" value={ inputPassword } name="inputPassword" onChange={ this.handleChange } />
+                    </label>
+                    <button type="button">Login</button>
+                </form>
+            </div>
         )
     }
 }
