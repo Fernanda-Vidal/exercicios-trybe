811dbc5bd700419fa641b6bcc285bbf80cd3d1ab
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-19 18:04:34 -0300

    Adiciona página Home com link para Login.

--- front-end/bloco-15-Gerenciamento-estado-redux/dia-15.3/exercicio/my-app-redux/src/App.js
+++ front-end/bloco-15-Gerenciamento-estado-redux/dia-15.3/exercicio/my-app-redux/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './redux/page/Home';
+import Login from './redux/page/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/login" component={ Login } />
+      </Switch>
+      </BrowserRouter>
   );
 }
 
---
+++ front-end/bloco-15-Gerenciamento-estado-redux/dia-15.3/exercicio/my-app-redux/src/redux/page/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello World</h1>
+            <Link to="/login">Login</Link>
+            </div>
+        )
+    }
+}
+
+export default Home;
