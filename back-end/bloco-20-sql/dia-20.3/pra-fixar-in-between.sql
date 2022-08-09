f7b553cdfb18e6181aa679c30bfa6936a003a7d3
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-09 18:51:53 -0300

    finaliza 20.3

--- back-end/bloco-20-sql/dia-20.3/pra-fixar-in-between.sql
+++ back-end/bloco-20-sql/dia-20.3/pra-fixar-in-between.sql
@@ -15,3 +15,26 @@ ORDER BY name;
 SELECT * FROM sakila.rental
 WHERE rental_date
 BETWEEN '2005-05-27' AND '2005-07-17';
+
+-- 1
+SELECT first_name, last_name, email FROM sakila.customer
+WHERE last_name IN ('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
+ORDER BY first_name;
+
+-- 2
+SELECT email FROM sakila.customer
+WHERE address_id BETWEEN 172 AND 176;
+
+-- 3
+SELECT COUNT(*) FROM sakila.rental
+WHERE rental_date BETWEEN '2005-05-01' AND '2005-08-01';
+
+-- 4
+SELECT * FROM sakila.film
+WHERE rental_duration BETWEEN 3 AND 6
+ORDER BY rental_duration DESC, title;
+
+-- 5
+SELECT * FROM sakila.film
+WHERE rating IN ('G', 'PG', 'PG-13')
+ORDER BY title;
