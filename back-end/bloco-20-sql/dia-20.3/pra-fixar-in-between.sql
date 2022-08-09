eec27a761bb2bc30799c5b439add4814a984450c
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-09 11:08:34 -0300

    pra fixar in

---
+++ back-end/bloco-20-sql/dia-20.3/pra-fixar-in-between.sql
@@ -0,0 +1,10 @@
+-- PRA FIXAR ## IN
+
+SELECT * FROM sakila.payment
+WHERE customer_id IN ('269', '239', '126', '399', 142);
+
+SELECT * FROM sakila.address
+WHERE district IN ('QLD', 'Nagasaki', 'California', 'Attika', 'Mandalay', 'Nantou', 'Texas');
+
+-- PRA FIXAR ## BETWEEN
+
