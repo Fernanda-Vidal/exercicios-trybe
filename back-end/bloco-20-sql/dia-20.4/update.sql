b510c4fb6cfae908b171d5ee008efe2be9409371
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-10 15:03:03 -0300

    add conteudo UPDATE

---
+++ back-end/bloco-20-sql/dia-20.4/update.sql
@@ -0,0 +1,50 @@
+-- UPDATE
+
+UPDATE sakila.staff
+SET first_name = 'Rannveig'
+WHERE first_name = 'Ravein';
+
+UPDATE nome_da_tabela
+SET propriedade_a_ser_alterada = 'novo valor para coluna'
+WHERE alguma_condicao; -- importantíssimo aplicar o WHERE para não alterar a tabela inteira!
+
+-- SAFE UPDATES MODE
+-- para desativar: rode o seguinte comando em uma janela de query dentro do MySQL Workbench sempre
+-- que abri-lo
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE sakila.staff
+SET first_name = 'Rannveig', last_name = 'Jordan'
+WHERE staff_id = 4;
+
+
+-- UPDATA EM MASSA
+-- Opção 1 - Incluindo a lista de condições fixas
+UPDATE sakila.actor
+SET first_name = 'JOE'
+WHERE actor_id IN (1,2,3);
+
+-- Opção 2 - Especificando como cada entrada será alterada individualmente
+UPDATE sakila.actor
+SET first_name = (
+CASE actor_id WHEN 1 THEN 'JOE' -- se actor_id = 1, alterar first_name para 'JOE'
+              WHEN 2 THEN 'DAVIS' -- se actor_id = 2, alterar first_name para 'DAVIS'
+              WHEN 3 THEN 'CAROLINE' -- se actor_id = 3, alterar first_name para 'CAROLINE'
+	      ELSE first_name -- em todos os outros casos, mantém-se o first_name
+END);
+
+-- UPDATA DE FORMA SEQUENCIAL
+-- valores entre colchetes ([]) são opcionais
+
+UPDATE nome_da_tabela
+SET coluna1 = valor1, coluna2 = valor2
+[WHERE condições]
+[ORDER BY expressao [ ASC | DESC ]]
+[LIMIT quantidade_resultados];
+
+-- Exemplo:
+UPDATE sakila.staff
+SET password = 'FavorResetarSuaSenha123'
+WHERE active = 1
+ORDER BY last_update
+LIMIT 2;
