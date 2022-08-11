0dca8ccad97493129523ca8f083b595712a70a21
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-11 19:47:31 -0300

    add conteudo funcoes matematicas do MySql

---
+++ back-end/bloco-21-funcoesSQL-JOINs-Normalizacao/funcoes-matematicas-no-SQL.sql
@@ -0,0 +1,24 @@
+-- Desafios com DIV e MOD
+
+-- 1
+SELECT IF(15 MOD 2 = 0, 'Par', 'Ímpar') AS `Par ou Ímpar`;
+
+-- 2
+SELECT 220 DIV 12;
+
+-- 3
+SELECT 220 MOD 12;
+
+-- PRA FIXAR
+
+-- 1
+SELECT ROUND(15 + (RAND() * 5));
+
+-- 2
+SELECT ROUND(15.7515971, 5);
+
+-- 3
+SELECT FLOOR(39.494);
+
+-- 4
+SELECT CEIL(85.234);
