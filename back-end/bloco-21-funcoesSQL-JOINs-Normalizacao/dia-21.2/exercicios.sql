2a0e604838e6ca1a4e22ec2adb8086c849b18292
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-12 20:53:55 -0300

    add exercicio 2

--- back-end/bloco-21-funcoesSQL-JOINs-Normalizacao/dia-21.2/exercicios.sql
+++ back-end/bloco-21-funcoesSQL-JOINs-Normalizacao/dia-21.2/exercicios.sql
@@ -4,3 +4,12 @@ movie.title, sales.domestic_sales, sales.international_sales
 FROM pixar.box_office AS sales
 INNER JOIN pixar.movies AS movie
 	ON sales.movie_id = movie.id;
+    
+-- EXERCICIO 2
+SELECT
+movie.title,
+sales.domestic_sales + sales.international_sales AS total
+FROM pixar.box_office AS sales
+INNER JOIN pixar.movies AS movie
+	ON sales.movie_id = movie.id
+WHERE sales.international_sales > sales.domestic_sales;
