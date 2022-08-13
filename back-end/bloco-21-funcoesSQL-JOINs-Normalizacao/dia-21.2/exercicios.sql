5963779c2f8ff5f34843a2576cf8d27d0e7f84ac
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-12 21:02:37 -0300

    add exercicio 3

--- back-end/bloco-21-funcoesSQL-JOINs-Normalizacao/dia-21.2/exercicios.sql
+++ back-end/bloco-21-funcoesSQL-JOINs-Normalizacao/dia-21.2/exercicios.sql
@@ -13,3 +13,11 @@ FROM pixar.box_office AS sales
 INNER JOIN pixar.movies AS movie
 	ON sales.movie_id = movie.id
 WHERE sales.international_sales > sales.domestic_sales;
+
+-- EXERCICIO 3
+SELECT
+mo.title, bo.rating AS nota
+FROM pixar.movies AS mo
+INNER JOIN pixar.box_office AS bo 
+	ON mo.id = bo.movie_id
+ORDER BY nota DESC;
