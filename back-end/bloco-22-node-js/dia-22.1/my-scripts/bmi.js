75edb151d84a21ab998351f99c5a027488ee53b8
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-22 10:44:13 -0300

    Add exercicio 3.

--- back-end/bloco-22-node-js/dia-22.1/my-scripts/bmi.js
+++ back-end/bloco-22-node-js/dia-22.1/my-scripts/bmi.js
@@ -1,17 +1,19 @@
-const weight = 68;
-const height = 169;
+const readline = require('readline-sync');
+
 
 const handleBMI = (weight, height) => {
     console.log(`Weight: ${weight}, Height: ${height}`);
-
+    
     const heightInM = height / 100;
     const finalHeigth = heightInM ** 2;
-
+    
     const bmi = weight / finalHeigth;
     return bmi;
 }
 
 const main = () => {
+    const weight = readline.questionInt('What your weight in kg?');
+    const height = readline.questionInt('What your height in cm?');
     const bmi = handleBMI(weight, height)
 
     console.log(`BMI: ${bmi.toFixed(2)}`)
