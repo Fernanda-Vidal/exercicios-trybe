7bf13da2e58f747856e6ab38720e5c3ec8b7c9b5
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-22 11:14:47 -0300

    Add exercicio 5.

--- back-end/bloco-22-node-js/dia-22.1/my-scripts/bmi.js
+++ back-end/bloco-22-node-js/dia-22.1/my-scripts/bmi.js
@@ -16,7 +16,13 @@ const main = () => {
     const height = readline.questionInt('What your height in cm?');
     const bmi = handleBMI(weight, height)
 
-    console.log(`BMI: ${bmi.toFixed(2)}`)
+    if (bmi <= 18.5) {  console.log(`BMI: ${bmi.toFixed(2)} - Abaixo do peso (magreza)`)}
+    else if (bmi >= 18.5 && bmi <= 24.9) {  console.log(`BMI: ${bmi.toFixed(2)} - Peso normal`)}
+    else if (bmi >= 25.0 && bmi <= 29.9) {  console.log(`BMI: ${bmi.toFixed(2)} - Acima do peso (sobrepeso)`)}
+    else if (bmi >= 30.0 && bmi <= 34.9) {  console.log(`BMI: ${bmi.toFixed(2)} - Obesidade grau I`)}
+    else if (bmi >= 35.0 && bmi <= 39.9) {  console.log(`BMI: ${bmi.toFixed(2)} - Obesidade grau II`)}
+    else  console.log(`BMI: ${bmi.toFixed(2)} - Obesidade grau III e IV`)
+    
 }
 
 main();
