6e23f6df287fc937381161f837da2df88c16b056
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-22 14:16:51 -0300

    Add 6b.

--- back-end/bloco-22-node-js/dia-22.1/my-scripts/simpsons.js
+++ back-end/bloco-22-node-js/dia-22.1/my-scripts/simpsons.js
@@ -2,11 +2,30 @@ const fs = require('fs').promises;
 
 const read = async () => await fs.readFile('simpsons.json', 'utf-8');
 
+// const main = async () => {
+//     const fileContent = await read();
+//     const simpsons = JSON.parse(fileContent);
+//     const returnString = simpsons.map(({ id, name }) => `${id} - ${name}`)
+//     console.log(returnString);
+
+//     return simpsons;
+// }
+
+const dataCharacter = async (id) =>  {
+        const fileContent = await read();
+        const simpsons = JSON.parse(fileContent)
+        const simpsonId = simpsons.find((character) => Number(character.id) === id)
+        
+        if(!simpsonId) {
+            throw new Error('id não encontrado');
+        }
+        
+        return simpsonId;
+}
+
 const main = async () => {
-    const fileContent = await read();
-    const simpsons = JSON.parse(fileContent);
-    const returnString = simpsons.map(({ id, name }) => `${id} - ${name}`)
-    console.log(returnString);
+    const simpson = await dataCharacter(2);
+    console.log(simpson);
 }
 
 main();
