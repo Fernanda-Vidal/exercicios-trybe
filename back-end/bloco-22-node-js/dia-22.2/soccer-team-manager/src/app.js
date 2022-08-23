c2c48a239e0cbcb6fa00e699fa2392eb230a8aec
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-23 15:39:30 -0300

    finaliza conteúdo.

--- back-end/bloco-22-node-js/dia-22.2/soccer-team-manager/src/app.js
+++ back-end/bloco-22-node-js/dia-22.2/soccer-team-manager/src/app.js
@@ -26,4 +26,35 @@ app.post('/teams', (req, res) => {
     res.status(201).json({ team: newTeam });
 });
 
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+app.get('/teams/:id', (req, res) => {
+  const team = teams.find(({ id }) => id === Number(req.params.id));
+  res.status(200).json(team);
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
+
 module.exports = app;
