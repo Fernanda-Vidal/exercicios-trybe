b7b1af26f07a2ed2f616a12ab5e68be81d1b509b
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-26 13:51:51 -0300

    Add exercicio 1 parte 7.

--- back-end/bloco-22-node-js/dia-22.4/startup-ecoturismo/src/routes/activitiesRoutes.js
+++ back-end/bloco-22-node-js/dia-22.4/startup-ecoturismo/src/routes/activitiesRoutes.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
+const createdAtValidation = require('../middlewares/createdAtValidation');
 const descriptionValidation = require('../middlewares/descriptionValidation');
 const nameValidation = require('../middlewares/nameValidation');
 const priceValidation = require('../middlewares/priceValidation');
 
 const routerActivities = Router();
 
-routerActivities.post('/activities', nameValidation, priceValidation, descriptionValidation, (req, res) => {
+routerActivities.post('/activities',
+    nameValidation,
+    priceValidation,
+    descriptionValidation,
+    createdAtValidation, (req, res) => {
 
     res.status(201).json({ message: "Atividade cadastrada com sucesso!" });
 })
