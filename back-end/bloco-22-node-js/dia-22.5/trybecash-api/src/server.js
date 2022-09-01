60e3e03870649b4948bf2b1eaf2e1de36f04bcd8
Fernanda Vidal <fevidal.dev@gmail.com>
2022-09-01 12:34:14 -0300

    Minha primeira conexão com o MySQL.

--- back-end/bloco-22-node-js/dia-22.5/trybecash-api/src/server.js
+++ back-end/bloco-22-node-js/dia-22.5/trybecash-api/src/server.js
@@ -1,1 +1,14 @@
-console.log('API TrybeCash está sendo executada na porta 3001')
+const app = require('./app');
+const connection = require('./db/connection');
+
+const port = 3001
+
+app.listen(port, async () => {
+    console.log(`API TrybeCash está sendo executada na porta ${port}`)
+
+    // Código que testa a comunicação com o MySQL
+    const [ result ] = await connection.execute('SELECT 1');
+    if (result) {
+        console.log('MySQL connection Ok');
+    }
+});
