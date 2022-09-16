6be4843ebcddc6e9497a75f8bdad11f42ba8278a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-09-16 19:01:39 -0300

    conteudo 24.1

---
+++ back-end/bloco-24-ORM-e-autenticacao/dia-24.1/app-with-sequelize/src/config/config.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const config = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
+
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
