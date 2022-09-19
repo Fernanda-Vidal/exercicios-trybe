43be19ea84aadd6dac0c33b49eb3bcc78c150aaa
Fernanda Vidal <fevidal.dev@gmail.com>
2022-09-19 16:21:43 -0300

    Managed transactions.

--- back-end/bloco-24-ORM-e-autenticacao/dia-24.3/orm-transactions/src/services/employee.service.js
+++ back-end/bloco-24-ORM-e-autenticacao/dia-24.3/orm-transactions/src/services/employee.service.js
@@ -23,29 +23,54 @@ const getById = async (id) => {
 
 const insert = async ({ firstName, lastName, age, city, street, number }) => {
     const t = await sequelize.transaction();
-    try {
+    //******UNMANEGED TRANACTION ***********/
+    // try {
     // Depois executamos as operações
-        const employee = await Employee.create(
-            { firstName, lastName, age },
-            { transaction: t },
-            );
+        // const employee = await Employee.create(
+        //     { firstName, lastName, age },
+        //     { transaction: t },
+        //     );
             
-            await Address.create(
-                { city, street, number, employeeId: employee.id },
-                { transaction: t },
-                );
+            // await Address.create(
+            //     { city, street, number, employeeId: employee.id },
+            //     { transaction: t },
+            //     );
 
     // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
     // Com isso, podemos finalizar a transação usando a função `commit`.
-            await t.commit();
-            return employee;
-        } catch (e) {
+        //     await t.commit();
+        //     return employee;
+        // } catch (e) {
     // Se entrou nesse bloco é porque alguma operação falhou.
     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente        
-            await t.rollback();
-            console.log(e);
-            throw e; // Jogamos o erro para a controller tratar
-        } 
+        //     await t.rollback();
+        //     console.log(e);
+        //     throw e; // Jogamos o erro para a controller tratar
+        // } 
+
+        // *********MANAGED TRANSACTIONS*************
+    try {
+        // Depois executamos as operações
+    const result = await sequelize.transaction(async (t) => {
+      const employee = await Employee.create({
+        firstName, lastName, age
+      }, { transaction: t });
+
+      await Address.create({
+        city, street, number, employeeId: employee.id
+      }, { transaction: t });
+      return employee;
+    });
+    return result;
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    console.log(e);
+    throw e; // Jogamos o erro para a controller tratar
+  }
   };
 
 module.exports = {
