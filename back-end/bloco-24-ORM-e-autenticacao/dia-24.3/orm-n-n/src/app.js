e00a037a3a8b6bcd18aed6a588295927a0cab604
Fernanda Vidal <fevidal.dev@gmail.com>
2022-09-21 17:35:33 -0300

    Outras alterações.

--- back-end/bloco-24-ORM-e-autenticacao/dia-24.3/orm-n-n/src/app.js
+++ back-end/bloco-24-ORM-e-autenticacao/dia-24.3/orm-n-n/src/app.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 
 app.get('/userbooks/:id', userBookController.getUserBooksById);
-app.get('/userbooks', userBookController.getUser);
+// app.get('/userbooks', userBookController.getUser);
 
 module.exports = app;
