a3c33f38227a63a9b9eb1d2f4078e41516f8cde0
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-03 15:33:47 -0300

    feat: implementa primeiras funções ts.

---
+++ back-end/sessao-8-introducao-typeScript/exercises/exercises.ts
@@ -0,0 +1,17 @@
+import exp from "constants";
+
+export function greeter(name:string):string {
+    return  `Olá ${ name }!`;
+}
+
+export function personAge(name: string, age: number): string {
+    return `${name} tem ${age} anos!`;
+}
+
+export function add(x: number, y: number): number {
+    return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+    return numbers.reduce(add, 0);
+}
