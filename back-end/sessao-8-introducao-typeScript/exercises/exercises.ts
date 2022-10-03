a55c1c9ffea0463c484247b9b5cd61c0e6088c59
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-03 15:37:58 -0300

    chore: finaliza exercises.ts

--- back-end/sessao-8-introducao-typeScript/exercises/exercises.ts
+++ back-end/sessao-8-introducao-typeScript/exercises/exercises.ts
@@ -15,3 +15,15 @@ export function add(x: number, y: number): number {
 export function sumArray(numbers: number[]): number {
     return numbers.reduce(add, 0);
 }
+
+export function triangle(base: number, height: number): number {
+    return (base * height) / 2;
+}
+
+export function square(side: number): number {
+    return side ** 2;
+}
+
+export function rectangle(base: number, height: number): number {
+    return base * height;
+}
