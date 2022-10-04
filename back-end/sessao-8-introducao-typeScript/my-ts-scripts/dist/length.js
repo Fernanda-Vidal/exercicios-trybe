56478a1f9689b6ac7cd5d3e6fac3d9fa3bcc85ca
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-04 10:29:06 -0300

    feat: exercise 8.

--- back-end/sessao-8-introducao-typeScript/my-ts-scripts/dist/length.js
+++ back-end/sessao-8-introducao-typeScript/my-ts-scripts/dist/length.js
@@ -1,4 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync"));
 const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
 function convert(value, from, to) {
     const fromIndex = units.indexOf(from);
@@ -7,3 +12,14 @@ function convert(value, from, to) {
     return value * Math.pow(10, exponent);
 }
 console.log(convert(20, 'm', 'km'));
+function exec() {
+    const value = readline_sync_1.default.questionFloat('Digite o valor a ser convertido: \n');
+    const fromUnitChoiceIndex = readline_sync_1.default.keyInSelect(units, 'Escolha um número para a unidade base:');
+    const toUnitChoiceIndex = readline_sync_1.default.keyInSelect(units, 'Escolha um número para conversão:');
+    const fromUnitChoice = units[fromUnitChoiceIndex];
+    const toUnitChoice = units[toUnitChoiceIndex];
+    const result = convert(value, fromUnitChoice, toUnitChoice);
+    const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}.`;
+    console.log(message);
+}
+exec();
--- back-end/sessao-8-introducao-typeScript/my-ts-scripts/length.ts
+++ back-end/sessao-8-introducao-typeScript/my-ts-scripts/length.ts
@@ -1,3 +1,5 @@
+import readline from 'readline-sync';
+
 const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"]
 
 function convert (value: number, from: string, to: string): number {
@@ -9,3 +11,18 @@ function convert (value: number, from: string, to: string): number {
 }
 
 console.log(convert(20, 'm', 'km'))
+
+function exec() {
+    const value = readline.questionFloat('Digite o valor a ser convertido: \n');
+    const fromUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para a unidade base:');
+    const toUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para conversão:');
+
+    const fromUnitChoice = units[fromUnitChoiceIndex];
+    const toUnitChoice = units[toUnitChoiceIndex];
+
+    const result = convert(value, fromUnitChoice, toUnitChoice);
+    const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}.`
+    console.log(message);
+}
+
+exec();
