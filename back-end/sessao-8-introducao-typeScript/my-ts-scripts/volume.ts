242c21174aa778d42a4d3026de7f12f27ff06f6b
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-04 11:27:54 -0300

    feat: exec volume.

--- back-end/sessao-8-introducao-typeScript/my-ts-scripts/volume.ts
+++ back-end/sessao-8-introducao-typeScript/my-ts-scripts/volume.ts
@@ -1,9 +1,26 @@
-const unitsVolume = ["km³", "hm³", "dam³", "m³", "dm³", "cm³", "mm³"];
+import readline from 'readline-sync';
 
-function convertVolume (value: number, from: string, to: string): number {
-    const fromIndex = unitsVolume.indexOf(from);
-    const toIndex = unitsVolume.indexOf(to);
+const units = ["km³", "hm³", "dam³", "m³", "dm³", "cm³", "mm³"];
+
+function convert (value: number, from: string, to: string): number {
+    const fromIndex = units.indexOf(from);
+    const toIndex = units.indexOf(to);
     const exponent = (toIndex - fromIndex);
 
     return value * Math.pow(10, exponent);
 }
+
+function exec() {
+    const value = readline.questionFloat('Digite o valor a ser convertido: \n');
+    const fromUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para a unidade base:');
+    const toUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para conversão:');
+
+    const fromUnitChoice = units[fromUnitChoiceIndex];
+    const toUnitChoice = units[toUnitChoiceIndex];
+
+    const result = convert(value, fromUnitChoice, toUnitChoice);
+    const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}.`
+    console.log(message);
+}
+
+exec();
