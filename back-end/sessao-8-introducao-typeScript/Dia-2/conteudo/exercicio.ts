a0cedff58a6dee69382b5e61c96f990cb87a8040
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-05 12:27:43 -0300

    feat: Generics

--- back-end/sessao-8-introducao-typeScript/Dia-2/conteudo/exercicio.ts
+++ back-end/sessao-8-introducao-typeScript/Dia-2/conteudo/exercicio.ts
@@ -186,3 +186,50 @@ type Address = {
   
   user.name;
   address.street;
+
+//   Conteúde GENERICS - Dar preferência para generics ao invés de type assertion
+// Refatorando a função acima
+function stringToJson<T>(str: string): T {
+    const result = JSON.parse(str);
+    return result;
+  }
+
+const user = stringToJson<User>('{"name":"André Soares","email":"andre@trybe.com","password":"senha_secreta"}');
+const address = stringToJson<Address>('{"street":"Rua Tamarindo","number":1}');
+
+user.name;
+address.street;
+
+// O genérics facilita na hora de refatorar e evita erros.
+
+function stringToJson<T, U>(str: string, id: U): T & { id: U } {
+    const result = JSON.parse(str);
+    result.id = id;
+    return result;
+  }
+
+const user = stringToJson<User, number>('{"name":"André Soares","email":"andre@trybe.com","password":"senha_secreta"}', Date.now());
+const address = stringToJson<Address, string>('{"street":"Rua Tamarindo","number":1}', '#01');
+
+
+function identity<T, U> (value: T, message: U) : T {
+    console.log(message);
+    return value
+}
+
+const returnNumber = identity<number, string>(100, "Olá");
+const returnString = identity<string, string>("100", "Mundo");
+const returnBoolean = identity<boolean, string>(true, "Olá, Mundo!");
+
+
+function getArray<T>(items : T[]) : T[] {
+    return new Array<T>().concat(items); // construindo um Array que só pode conter elementos do tipo T
+  }
+  
+  const numberArray = getArray<number>([5, 10, 15, 20]);
+  numberArray.push(25);
+  numberArray.push("This is not a number"); // Isto vai gerar um erro de compilação
+  
+  const stringArray = getArray<string>(["Cats", "Dogs", "Birds"]);
+  stringArray.push("Rabbits");
+  stringArray.push(30); // Isto vai gerar um erro de compilação
