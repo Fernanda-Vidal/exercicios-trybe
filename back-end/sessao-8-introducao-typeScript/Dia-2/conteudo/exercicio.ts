5c0c5aefb2ddea82d88ae02bfdb4c55531607407
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-05 12:08:25 -0300

    feat: Assertions

--- back-end/sessao-8-introducao-typeScript/Dia-2/conteudo/exercicio.ts
+++ back-end/sessao-8-introducao-typeScript/Dia-2/conteudo/exercicio.ts
@@ -159,3 +159,30 @@ let teacher: Teacher = {
 }
 
 console.log(teacher);
+
+// Conteudo TYPE ASSERTION
+type Address = {
+    street: string,
+    number: number | null,
+  }
+  
+  type User = {
+    name: string,
+    email: string,
+    password: string,
+  }
+  
+  // função que converte de string para json
+  function stringToJson(str: string): unknown {
+    const result = JSON.parse(str);
+    return result;
+  }
+  
+  // utilizando o "as" para converter de unknown para User
+  const user = stringToJson('{"name":"André Soares","email":"andre@trybe.com","password":"senha_secreta"}') as User
+  
+  // Outra forma de usar o Assertion. A sintaxe é diferente mas tem o mesmo objetivo
+  const address = <Address> stringToJson('{"street":"Rua Tamarindo","number":1}')
+  
+  user.name;
+  address.street;
