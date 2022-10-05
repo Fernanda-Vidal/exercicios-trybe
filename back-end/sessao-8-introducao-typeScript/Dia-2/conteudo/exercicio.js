3d8e31ab08733109c10bf31e6b4e5963cae1b8fb
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-05 10:06:36 -0300

    feat: conteudo e pra fixar.

---
+++ back-end/sessao-8-introducao-typeScript/Dia-2/conteudo/exercicio.js
@@ -0,0 +1,5 @@
+function printCoord(pt) {
+    console.log('O valor da coordenada x é:' + pt.x);
+    console.log('O valor da coordenada y é:' + pt.y);
+}
+printCoord({ x: 200, y: 100 });
---
+++ back-end/sessao-8-introducao-typeScript/Dia-2/conteudo/exercicio.ts
@@ -0,0 +1,29 @@
+// Conteudo
+type Point = {
+    x: number,
+    y: number,
+};
+
+function printCoord(pt: Point) {
+    console.log('O valor da coordenada x é:' + pt.x);
+    console.log('O valor da coordenada y é:' + pt.y);
+}
+
+printCoord({ x: 200, y: 100 })
+
+// Pra fixar
+type Bird = {
+    wings: 2,
+    beaks: 1,
+    biped: true,
+};
+
+type Sum = (x: number, y: number) => number;
+
+type Address = {
+    street: string,
+    number: string, 
+    district: string, 
+    city: string,
+    state: string,
+};
