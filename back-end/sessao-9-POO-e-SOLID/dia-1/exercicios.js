deb1f73578ff01bf4187322168fda3661f689f0b
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-11 16:43:29 -0300

    pra fixar II.

--- back-end/sessao-9-POO-e-SOLID/dia-1/exercicios.js
+++ back-end/sessao-9-POO-e-SOLID/dia-1/exercicios.js
@@ -22,3 +22,42 @@ var TV = /** @class */ (function () {
 var tv1 = new TV("LG", 32, "Full HD", "HDMI");
 console.log(tv1);
 tv1.turnOn();
+// Altere a visibilidade dos atributos definidos na classe Tv para private.
+// Crie um método getter e um setter para o atributo _connectedTo, da classe Tv.
+// O setter deverá verificar se o valor definido está entre as conexões disponíveis (_connections) e:
+// Em caso positivo, definir este valor para o atributo _connectedTo;
+// Em caso negativo, imprimir no console a mensagem “Sorry, connection unavailable!”
+// Defina um valor para o atributo _connectedTo por meio do método setter criado e imprima seu valor.
+var TV2 = /** @class */ (function () {
+    function TV2(b, s, r, cT) {
+        this._brand = b;
+        this._size = s;
+        this._resolution = r;
+        this._connection = ["HDMI", "Ethernet"];
+        this._connectedTo = cT;
+    }
+    TV2.prototype.turnOn = function () {
+        console.log("Turn on TV:".concat(this._brand, ", ").concat(this._size, ", ").concat(this._resolution, ", ").concat(this._connection, " ").concat(this._connectedTo));
+    };
+    Object.defineProperty(TV2.prototype, "connectedTo", {
+        // get connectedTo() {
+        //     return this._connectedTo as string;
+        // }
+        set: function (conn) {
+            if (this._connection.includes(conn)) {
+                this._connectedTo = conn;
+            }
+            else {
+                console.log("Sorry, connection unavailable");
+            }
+            ;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return TV2;
+}());
+var tvOi = new TV2("LG", 32, "Full HD");
+// console.log(tvOi.connectedTo);
+tvOi.connectedTo = 'HDMI';
+console.log(tvOi.connectedTo);
--- back-end/sessao-9-POO-e-SOLID/dia-1/exercicios.ts
+++ back-end/sessao-9-POO-e-SOLID/dia-1/exercicios.ts
@@ -32,3 +32,51 @@ const tv1 = new TV("LG", 32, "Full HD", "HDMI");
 
 console.log(tv1)
 tv1.turnOn();
+
+// Altere a visibilidade dos atributos definidos na classe Tv para private.
+
+// Crie um método getter e um setter para o atributo _connectedTo, da classe Tv.
+
+// O setter deverá verificar se o valor definido está entre as conexões disponíveis (_connections) e:
+// Em caso positivo, definir este valor para o atributo _connectedTo;
+// Em caso negativo, imprimir no console a mensagem “Sorry, connection unavailable!”
+// Defina um valor para o atributo _connectedTo por meio do método setter criado e imprima seu valor.
+
+class TV2 {
+    private _brand: string;
+    private _size: number;
+    private _resolution: string;
+    private _connection: string[];
+    private _connectedTo?: string;
+
+    constructor (b: string, s: number, r: string, cT?: string) {
+        this._brand = b;
+        this._size = s;
+        this._resolution = r;
+        this._connection = ["HDMI", "Ethernet"];
+        this._connectedTo = cT;
+    }
+
+    turnOn() {
+        console.log(`Turn on TV:${this._brand}, ${this._size}, ${this._resolution}, ${this._connection} ${this._connectedTo}`);
+    }
+
+    // get connectedTo() {
+    //     return this._connectedTo as string;
+    // }
+
+    set connectedTo(conn: string) {
+        if (this._connection.includes(conn)) {
+            this._connectedTo = conn;
+        }
+        else {
+            console.log("Sorry, connection unavailable")
+        };
+    }
+}
+
+const tvOi = new TV2("LG", 32, "Full HD")
+
+// console.log(tvOi.connectedTo);
+tvOi.connectedTo = 'HDMI'
+console.log(tvOi.connectedTo);
