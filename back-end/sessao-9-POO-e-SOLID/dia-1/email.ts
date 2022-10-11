ffa75962303948090e4226cc3708ab2edd590aec
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-11 20:55:13 -0300

    feat: class mailList.

--- back-end/sessao-9-POO-e-SOLID/dia-1/email.ts
+++ back-end/sessao-9-POO-e-SOLID/dia-1/email.ts
@@ -36,3 +36,29 @@ class Email {
         ${this._message}`
     }
 }
+
+class MailList {
+  //  Essa sintaxe no construtor é chamada `Parameter Properties`
+  // É um atalho para declarar e automaticamente atribuir o valor que será recebido via parâmetro ao atributo privado `mailList`
+    constructor(private mailList: Email[] = []) {}
+
+    get all(): Email[] { return this.mailList; }
+
+    getEmailsSentBy(mailAddress: string): Email[] {
+        return this.mailList.filter((mail) => mail.from === mailAddress);
+    }
+
+    getEmailsTo(mailAddress: string): Email[] {
+        return this.mailList.filter((mail) => mail.to === mailAddress);
+    }
+
+    getEmailsBySubject(searchString:string): Email[] {
+        return this.mailList.filter((mail) => mail.subject.indexOf(searchString) !== -1)
+    }
+
+    addEmail(newMail: Email): void { this.mailList.push(newMail); }
+
+    removeEmail(mailToRemove: Email): void {
+        this.mailList = this.mailList.filter((mail) => mail !== mailToRemove);
+    }
+}
