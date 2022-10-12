8f0d6c1552dbc90c11c1720c980be7e4b704890a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-12 15:38:21 -0300

    sintaxe herança.

---
+++ back-end/sessao-9-POO-e-SOLID/dia-2/conteudo/Animal.ts
@@ -0,0 +1,23 @@
+class Animal {
+    constructor( public name:string, private birthDate: Date) {}
+
+    get age() {
+        const timeDiff = Math.abs(
+            Date.now() - new Date(this.birthDate).getTime()
+        );
+        return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25)
+    }
+}
+
+class Mammal extends Animal {
+    walk() {
+        console.log(`${this.name} está andando`);
+    }
+}
+
+const tiger = new Mammal('Tigre', new Date(Date.parse('May 03, 2021')));
+
+const main = (animal: Animal) => console.log(animal.age);
+
+main(tiger);
+tiger.walk();
