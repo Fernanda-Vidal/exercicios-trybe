6280a0b60afc318827f7adb82f213723d5678e4f
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-18 15:29:15 -0300

    feat: Liskov Substitution Principle.

---
+++ back-end/sessao-9-POO-e-SOLID/dia-5/conteudo/src/Connectors/Connector.ts
@@ -0,0 +1,19 @@
+export interface ConnectorConstructor {
+    host: string;
+    port: number;
+    database?: string;
+    user?: string;
+    password?: string;
+  }
+
+  export interface Connector {
+  getCount(token: string): number | Promise<number>;
+
+  incrementCount(token: string): void;
+  
+  closeConnection(): void;
+
+  clearCount(token: string): void;
+
+  firstCount(token: string): void;
+  }
---
+++ back-end/sessao-9-POO-e-SOLID/dia-5/conteudo/src/Connectors/index.ts
@@ -0,0 +1,12 @@
+import { Connector, ConnectorConstructor } from "./Connector";
+import MySQLConnector from "./mysql/MySQLConnector";
+import RedisConnector from "./redis/RedisConnector";
+
+export default Connector;
+
+export {
+    Connector,
+    ConnectorConstructor,
+    MySQLConnector,
+    RedisConnector
+};
