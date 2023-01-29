3b6372201b223cd7fd44a7a0dc5afdd0c692b814
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-29 16:59:39 -0300

    feat: testando falhas.

--- CS/sessao-1-phyton/dia-3-testes/test_codigo.py
+++ CS/sessao-1-phyton/dia-3-testes/test_codigo.py
@@ -1,4 +1,5 @@
-from codigo import is_odd
+import pytest
+from codigo import is_odd, divide
 
 
 def test_is_odd_when_number_is_odd_returns_true():
@@ -10,3 +11,7 @@ def test_is_odd_when_number_is_even_returns_false():
     'Para um número par a função deve retornar o valor False'
     assert is_odd(2
     ) is False
+
+def test_divide_when_other_number_is_zero_raises_an_exception():
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        divide(3, 0)
