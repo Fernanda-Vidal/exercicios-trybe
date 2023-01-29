27275c4a25e84b258acf193d8899cc029fce72dc
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-29 16:50:56 -0300

    feat:

--- CS/sessao-1-phyton/dia-3-testes/test_codigo.py
+++ CS/sessao-1-phyton/dia-3-testes/test_codigo.py
@@ -8,4 +8,5 @@ def test_is_odd_when_number_is_odd_returns_true():
 
 def test_is_odd_when_number_is_even_returns_false():
     'Para um número par a função deve retornar o valor False'
-    assert is_odd(2) is False
+    assert is_odd(2
+    ) is False
