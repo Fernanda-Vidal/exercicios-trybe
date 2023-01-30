2d033860822f52ab5b84dc246772914acc2b6638
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-29 21:21:59 -0300

    feat: encapsulamento e abstração.

--- CS/sessao-2-padrao-de-projetos/dia-1-POO-em-python/poo_python.py
+++ CS/sessao-2-padrao-de-projetos/dia-1-POO-em-python/poo_python.py
@@ -23,3 +23,44 @@ seu_liquidificador = Liquidificador(preco=100, potencia=250, cor="Vermelho", ten
 # meio de parâmetros na chamada do construtor. Por exemplo, para o 
 # booleano __ligado e o inteiro __velocidade, o construtor vai iniciá-los 
 # sempre com os valores fixos (hard coded) False e 0, respectivamente.
+
+# Nomes iniciados com _ (underline): são considerados “protegidos“,
+# Nomes iniciados com __ (dunder/duplo underline): são considerados “privados“,
+
+class Liquidificador:
+    def __init__(self, cor, potencia, tensao, preco):
+        self.preco = preco
+        self.cor = cor
+        self._potencia = potencia
+        self._tensao = tensao
+        self.__ligado = False
+        self.__velocidade = 0
+        self.__velocidade_maxima = 3
+        self.__corrente_atual_no_motor = 0
+
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
+liquidificador_vermelho = Liquidificador("Vermelho", 250, 220, 100)
+liquidificador_vermelho.ligar(1)
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? True
+liquidificador_vermelho.desligar()
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? False
