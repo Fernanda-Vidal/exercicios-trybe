63d34b3b168473dcc01b0d7b98e2bcb1d516418d
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-31 08:24:10 -0300

    feat: Composição.

--- CS/sessao-2-padrao-de-projetos/dia-1-POO-em-python/poo_python.py
+++ CS/sessao-2-padrao-de-projetos/dia-1-POO-em-python/poo_python.py
@@ -59,8 +59,10 @@ class Liquidificador:
 
 liquidificador_vermelho = Liquidificador("Vermelho", 250, 220, 100)
 liquidificador_vermelho.ligar(1)
-print("Está ligado?", liquidificador_vermelho.esta_ligado())
-# Está ligado? True
-liquidificador_vermelho.desligar()
-print("Está ligado?", liquidificador_vermelho.esta_ligado())
-# Está ligado? False
+
+if __name__ == "__main__":
+    print("Está ligado?", liquidificador_vermelho.esta_ligado())
+    # Está ligado? True
+    liquidificador_vermelho.desligar()
+    print("Está ligado?", liquidificador_vermelho.esta_ligado())
+    # Está ligado? False
