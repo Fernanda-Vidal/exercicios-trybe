c7fd910c154bced1cdb12a5adad7d21b23b869fe
Fernanda Vidal <fevidal.dev@gmail.com>
2023-02-02 08:39:38 -0300

    feat: exercicio

---
+++ CS/sessao-2-padrao-de-projetos/dia-2-padroes/exercicio_iterator.py
@@ -0,0 +1,18 @@
+lista = (1, 2, 3)
+iter(lista)
+# <tuple_iterator object at 0x7fe1e8fe7cd0>
+other = ["a", "b", "c"]
+iter(other)
+# <tuple_iterator object at 0x7fe1e9028a00>
+obj_iter = iter(lista)
+next(obj_iter)
+# 1
+next(obj_iter)
+# 2
+next(obj_iter)
+# 3
+next(obj_iter)
+# StopIteration
+# >>>
+# KeyboardInterrupt
+# >>>
