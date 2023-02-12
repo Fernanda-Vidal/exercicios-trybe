fcf10df4b80fd9f9dc45d114f29e412bdaf7fd20
Fernanda Vidal <fevidal.dev@gmail.com>
2023-02-12 12:37:21 -0300

    feat: course - limpeza de dados

--- CS/sessao-3-raspagem-de-dados/dia-1-raspagem-dados/teste.py
+++ CS/sessao-3-raspagem-de-dados/dia-1-raspagem-dados/teste.py
@@ -27,3 +27,26 @@ detail_selector = Selector(text=detail_response.text)
 # ~ significa a tag irmã
 description = detail_selector.css("#product_description ~ p::text").get()
 print(description)
+
+# buscando todos os preços
+response = requests.get("http://books.toscrape.com/")
+selector = Selector(text=response.text)
+# Extrai todos os preços da primeira página
+prices = selector.css(".product_price .price_color::text").re(r"£\d+\.\d{2}")
+print(prices)
+
+# buscando todas as descrições
+response2 = requests.get(
+    "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+)
+selector = Selector(text=response2.text)
+
+# Extrai a descrição
+description = selector.css("#product_description ~ p::text").get()
+print(description)
+
+# "Fatiamos" a descrição removendo o sufixo
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[: -len(suffix)]
+print(description)
