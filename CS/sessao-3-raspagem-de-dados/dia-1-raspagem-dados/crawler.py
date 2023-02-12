a1f83cc6d4b70cbc9545b8dd141162023796cef3
Fernanda Vidal <fevidal.dev@gmail.com>
2023-02-12 11:45:23 -0300

    feat: course - analisando respostas

--- CS/sessao-3-raspagem-de-dados/dia-1-raspagem-dados/crawler.py
+++ CS/sessao-3-raspagem-de-dados/dia-1-raspagem-dados/crawler.py
@@ -1,4 +1,5 @@
 import requests
+from parsel import Selector
 import time
 
 for _ in range(15):
@@ -7,6 +8,7 @@ for _ in range(15):
     time.sleep(6)
 
 # Timeout
+
 try:
     response2 = requests.get("http://httpbin.org/delay/5", timeout=2)
 # except requests.Timeout: OU
@@ -14,3 +16,18 @@ except requests.ReadTimeout:
     response2 = requests.get("http://httpbin.org/delay/5", timeout=8)
 finally:
     print(response2.status_code)
+
+response3 = requests.get("http://books.toscrape.com/")
+selector = Selector(text=response3.text)
+
+print('só o primeiro', selector.css("img.thumbnail").get())
+
+print(selector.css("div.image_container a").getall())
+
+thumb_url_selector = "div.image_container a::attr(href)"
+
+for url in selector.css(thumb_url_selector).getall():
+    thumbnail_request = requests.get("http://books.toscrape.com/" + url)
+    thumbnail_selector = Selector(text=thumbnail_request.text)
+    book_title = thumbnail_selector.css("div.product_main h1")
+    print(book_title.get())
