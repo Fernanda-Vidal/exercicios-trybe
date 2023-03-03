0ece12c07b94cf20db3be3b12a9b5dd0355bc7a7
Fernanda Vidal <fevidal.dev@gmail.com>
2023-03-03 09:01:18 -0300

    feat:

---
+++ CS/sessao-5-estrutura-de-dados/dia-4-nos-e-listas-encadeadas/node.py
@@ -0,0 +1,115 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return f"Node(value={self.value}, next={self.next})"
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_value = None
+        self.__length = 0
+
+    def __str__(self) -> str:
+        return f"LinkedList(len={self.__length}, value={self.head_value})"
+
+    def __len__(self):
+        return self.__length
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        # Aqui o elemento atual é informado como o nexte para o nó e o atual
+        # está sendo inserido na cabeça.
+        first_value.next = self.head_value
+        self.head_value = first_value
+        self.__length += 1
+
+    def insert_last(self, value):
+        last_value = Node(value)
+        current_value = self.head_value
+
+        if current_value is None:
+            return self.insert_first(value)
+
+        while current_value.next:
+            current_value = current_value.next
+        current_value.next = last_value
+        self.__length += 1
+
+    def insert_at(self, value, position):
+        if position < 1:
+            return self.insert_first(value)
+        if position >= len(self):
+            return self.insert_last(value)
+        current_value = self.head_value
+        while position > 1:
+            current_value = current_value.next
+            position -= 1
+        next_value = Node(value)
+        next_value.next = current_value.next
+        current_value.next = next_value
+        self.__length += 1
+
+    def remove_first(self):
+        value_to_be_removed = self.head_value
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
+            self.__length -= 1
+        return value_to_be_removed
+
+    def remove_last(self):
+        if len(self) <= 1:
+            return self.remove_first()
+
+        previous_to_be_removed = self.head_value
+
+        while previous_to_be_removed.next.next:
+            previous_to_be_removed = previous_to_be_removed.next
+
+        value_to_be_removed = previous_to_be_removed.next
+        previous_to_be_removed.next = None
+        self.__length -= 1
+        return value_to_be_removed
+
+    def remove_at(self, position):
+        if position < 1:
+            return self.remove_first()
+        if position >= len(self):
+            return self.remove_last()
+
+        previous_to_be_removed = self.head_value
+        while position > 1:
+            previous_to_be_removed = previous_to_be_removed.next
+            position -= 1
+        value_to_be_removed = previous_to_be_removed.next
+        previous_to_be_removed.next = value_to_be_removed.next
+        value_to_be_removed.next = None
+        self.__length -= 1
+        return value_to_be_removed
+
+    def get_element_at(self, position):
+        value_returned = None
+        value_to_be_returned = self.head_value
+        if value_to_be_returned:
+            while position > 0 and value_to_be_returned.next:
+                value_to_be_returned = value_to_be_returned.next
+                position -= 1
+            if value_to_be_returned:
+                value_returned = Node(value_to_be_returned.value)
+        return value_returned
+
+
+# Um ponto de atenção para as verificações constantes presentes no código,
+# elas indicam que:
+# - Caso não haja elementos em nossa estrutura será retornado None;
+# - Caso a posição seja menor igual a 0, será retornado o primeiro elemento;
+# - Caso a posição seja maior igual a N, onde N é o tamanho da lista, será
+# retornado o último elemento.
+# Por fim, retornamos um novo Node com o mesmo valor do existente em nossa
+# estrutura. Isto é necessário para que retornemos apenas o valor, e não a
+# referência aos demais elementos.
+
+Está vazia
